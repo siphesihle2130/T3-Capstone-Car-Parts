dd7a77b1cd124aa3da10c891ba31fa4787e4da06
siphesihle <khinasesiphesihel@gmail.com>
2026-07-16 22:13:53 +0200

    edited addressRepository

--- src/main/java/za/ac/cput/carpartmarket/Repository/AddressRepository.java
+++ src/main/java/za/ac/cput/carpartmarket/Repository/AddressRepository.java
@@ -4,7 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import za.ac.cput.carpartmarket.Domain.Address;
 
+import java.util.List;
+
 @Repository
 public interface AddressRepository extends JpaRepository <Address, String> {
 
+    Address findByStreetNumber(String StreetNumber);
+
+    List<Address> findByProvince(String Province);
 }
