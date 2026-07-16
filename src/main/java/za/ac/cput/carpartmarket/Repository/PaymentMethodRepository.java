a19f86f7baec4c66e2ed814dd79941d57c72e1a0
siphesihle <khinasesiphesihel@gmail.com>
2026-07-16 22:13:15 +0200

    edited PaymentMethodRepository

--- src/main/java/za/ac/cput/carpartmarket/Repository/PaymentMethodRepository.java
+++ src/main/java/za/ac/cput/carpartmarket/Repository/PaymentMethodRepository.java
@@ -2,8 +2,14 @@ package za.ac.cput.carpartmarket.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import za.ac.cput.carpartmarket.Domain.Address;
 import za.ac.cput.carpartmarket.Domain.PaymentMethod;
+
+import java.util.List;
 
 @Repository
 public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, String> {
+    PaymentMethod findByCvv(int Cvv);
+
+    List<PaymentMethod> findByProvider(String Provider);
 }
